3df67235a10ed6f8ed202acb890dc7b7d520c0c4
Pranay Valson <pranay.valson@gmail.com>
2022-07-18 19:42:17 -0700

    adds contract observer as app
    Signed-off-by: Pranay Valson <pranay.valson@gmail.com>

---
+++ lib/contract_observer/application.ex
@@ -0,0 +1,30 @@
+defmodule ContractObserver.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  def start(_type, _args) do
+    # List all child processes to be supervised
+    children = [
+      # Start the endpoint when the application starts
+      ContractObserverWeb.Endpoint,
+      ContractObserver.EventHandler
+      # Starts a worker by calling: ContractObserver.Worker.start_link(arg)
+      # {ContractObserver.Worker, arg},
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: ContractObserver.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+  # Tell Phoenix to update the endpoint configuration
+  # whenever the application is updated.
+  def config_change(changed, _new, removed) do
+    ContractObserverWeb.Endpoint.config_change(changed, removed)
+    :ok
+  end
+end
