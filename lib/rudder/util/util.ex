41a287034d0538a32f7b82851c66332109db71dd
Pranay Valson <pranay.valson@gmail.com>
2022-10-04 19:34:56 -0700

    adds utilities to rudder
    Signed-off-by: Pranay Valson <pranay.valson@gmail.com>

---
+++ lib/rudder/util/util.ex
@@ -0,0 +1,21 @@
+defmodule Rudder.Util do
+
+  def typeof(a) do
+    cond do
+      is_float(a) -> "float"
+      is_number(a) -> "number"
+      is_atom(a) -> "atom"
+      is_boolean(a) -> "boolean"
+      is_binary(a) -> "binary"
+      is_function(a) -> "function"
+      is_list(a) -> "list"
+      is_tuple(a) -> "tuple"
+      true -> "idunno"
+    end
+  end
+
+  def file_open(path) do
+    Path.wildcard(path)
+  end
+
+end
