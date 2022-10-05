ba9fd13657a4a5faa82b67292a2460151c3b941a
Pranay Valson <pranay.valson@gmail.com>
2022-10-05 13:05:15 -0700

    renames module with noun<>verb syntax
    Signed-off-by: Pranay Valson <pranay.valson@gmail.com>

--- lib/rudder/application.ex
+++ lib/rudder/application.ex
@@ -13,8 +13,8 @@ defmodule Rudder.Application do
         start: {Rudder.SourceDiscovery, :start_link, [[]]}
       },
       %{
-        id: Rudder.Avro.DecodeBlockSpecimen,
-        start: {Rudder.Avro.DecodeBlockSpecimen, :start_link, [[]]}
+        id: Rudder.Avro.BlockSpecimenDecoder,
+        start: {Rudder.Avro.BlockSpecimenDecoder, :start_link, [[]]}
       },
       Rudder.Avro.Client,
       {Finch,
