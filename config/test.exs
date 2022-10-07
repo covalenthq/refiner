0301b93378b8e4fc7f803d65f330c7d1c87bfe68
kitti-katy <kitti-katy@mail.ru>
2022-10-07 14:33:41 -0400

    remove unused config

--- config/test.exs
+++ config/test.exs
@@ -1,7 +1,1 @@
 import Config
-
-# config :ethereumex, url: "http://0.0.0.0:8545"
-
-# config :ethereumex, ipc_path: "#{System.user_home!()}/.local/share/io.parity.ethereum/jsonrpc.ipc"
-
-# config :ethereumex, id_reset: true
