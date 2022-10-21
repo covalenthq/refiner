f0e5c759ad5e78c6ef620a8325e78ac742804a2e
kitti-katy <kitti-katy@mail.ru>
2022-10-21 14:32:03 -0400

    move extract_data function to Rudder.util

--- lib/rudder/util/util.ex
+++ lib/rudder/util/util.ex
@@ -16,4 +16,11 @@ defmodule Rudder.Util do
   def file_open(path) do
     Path.wildcard(path)
   end
+
+  def extract_data(block_data, signature) do
+    {:ok, data} = Map.fetch(block_data, "data")
+    "0x" <> data = data
+    fs = ABI.FunctionSelector.decode(signature)
+    ABI.decode(fs, data |> Base.decode16!(case: :lower))
+  end
 end
