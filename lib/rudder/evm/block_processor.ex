7f86d22c259768e666008a25bd2908b058e5ab5d
kitti-katy <kitti-katy@mail.ru>
2022-10-28 03:55:42 -0400

    add test for the pipeline, add file deletion after block result upload

--- lib/rudder/evm/block_processor.ex
+++ lib/rudder/evm/block_processor.ex
@@ -97,7 +97,7 @@ defmodule Rudder.BlockProcessor.Core do
 
     @impl true
     def handle_info(result = %Struct.ExecResult{}, state) do
-      GenServer.reply(result.misc, {result.status, result.block_id, result.output_path})
+      GenServer.reply(result.misc, {result.status, result.output_path})
 
       child_id = result.block_id
       Supervisor.terminate_child(:evm_pool_supervisor, child_id)
