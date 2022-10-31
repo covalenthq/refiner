ac0ff56140024b16da24178457d503d545655092
kitti-katy <kitti-katy@mail.ru>
2022-10-31 14:53:45 -0400

    refactor

--- lib/rudder/evm/block_processor_api.ex
+++ lib/rudder/evm/block_processor_api.ex
@@ -18,7 +18,7 @@ defmodule Rudder.BlockProcessor do
   @doc """
   Sends a synchronous request to the worker pool for manufacturing block result from the given block specimen contents
   """
-  def sync_queue({block_id, contents}) do
-    Core.Server.sync_queue({block_id, contents})
+  def sync_queue(block_specimen_metadata) do
+    Core.Server.sync_queue(block_specimen_metadata)
   end
 end
--- lib/rudder/pipeline.ex
+++ lib/rudder/pipeline.ex
@@ -10,44 +10,59 @@ defmodule Rudder.Pipeline do
     GenServer.start_link(__MODULE__, [], name: __MODULE__)
   end
 
+  defp fetch_replica_event(decoded_specimen) do
+    with {:ok, replica_event} <- Map.fetch(decoded_specimen, "replicaEvent") do
+      [replica_event | _] = replica_event
+      {:ok, replica_event}
+    else
+      err -> err
+    end
+  end
+
+  defp extract_block_specimen_metadata(decoded_specimen) do
+    with {:ok, block_height} <- Map.fetch(decoded_specimen, "startBlock"),
+         {:ok, replica_event} <- fetch_replica_event(decoded_specimen),
+         {:ok, data} <- Map.fetch(replica_event, "data"),
+         {:ok, chain_id} <- Map.fetch(data, "NetworkId") do
+      {:ok,
+       %Rudder.BlockSpecimenMetadata{
+         chain_id: chain_id,
+         block_height: Integer.to_string(block_height),
+         contents: Poison.encode!(data)
+       }}
+    else
+      err -> err
+    end
+  end
+
+  defp write_to_backlog(specimen_hash, urls, err) do
+    :ok
+  end
+
   # TODOs:
   # - keep track of duplicate specimens
   # - keep track of fail backlog
-  # - implement file clean up
-  # - fix block processor
   # - set up ProofChain for testing
   # - add tests
   def pipeline(specimen_hash, urls) do
     # urls = ["ipfs://QmR4BQi8fTdZM28GuWmtfjkbNPPzxiHCBNRXJng6rSEfcv"]
-    {:ok, specimen} = Rudder.BlockSpecimenDiscoverer.discover_block_specimen(urls)
-    {:ok, decoded_specimen} = Rudder.Avro.BlockSpecimenDecoder.decode(specimen)
-    {:ok, block_height} = Map.fetch(decoded_specimen, "startBlock")
-    block_height = Integer.to_string(block_height)
-    {:ok, replica_event} = Map.fetch(decoded_specimen, "replicaEvent")
-    [specimen | _] = replica_event
-    {:ok, data} = Map.fetch(specimen, "data")
-    {:ok, chain_id} = Map.fetch(data, "NetworkId")
-    specimen = Poison.encode!(data)
-    {:success, block_result_file_path} = Rudder.BlockProcessor.sync_queue({"block_id", specimen})
-
-    # Rudder.Pipeline.pipeline("a", [])
-
-    block_result_metadata = %Rudder.BlockResultMetadata{
-      chain_id: chain_id,
-      block_height: 1,
-      block_specimen_hash: specimen_hash,
-      file_path: block_result_file_path
-    }
-
-    IO.inspect(block_result_metadata)
-
-    {:ok, cid, block_result_hash} =
-      Rudder.BlockResultUploader.upload_block_result(block_result_metadata)
-
-    IO.inspect(cid)
-    IO.inspect(block_result_hash)
-
-    :ok = File.rm(block_result_file_path)
-    {:ok, cid, block_result_hash}
+    with {:ok, specimen} <- Rudder.BlockSpecimenDiscoverer.discover_block_specimen(urls),
+         {:ok, decoded_specimen} <- Rudder.Avro.BlockSpecimenDecoder.decode(specimen),
+         {:ok, block_specimen_metadata} <- extract_block_specimen_metadata(decoded_specimen),
+         {:success, block_result_file_path} <-
+           Rudder.BlockProcessor.sync_queue(block_specimen_metadata),
+         block_result_metadata <- %Rudder.BlockResultMetadata{
+           chain_id: block_specimen_metadata.chain_id,
+           block_height: block_specimen_metadata.block_height,
+           block_specimen_hash: specimen_hash,
+           file_path: block_result_file_path
+         },
+         {:ok, cid, block_result_hash} <-
+           Rudder.BlockResultUploader.upload_block_result(block_result_metadata),
+         :ok <- File.rm(block_result_file_path) do
+      {:ok, cid, block_result_hash}
+    else
+      err -> write_to_backlog(specimen_hash, urls, err)
+    end
   end
 end
