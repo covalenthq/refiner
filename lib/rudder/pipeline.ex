7410908991c2658ec428c009b1a6da2ab68f6df0
kitti-katy <kitti-katy@mail.ru>
2022-11-02 20:58:38 -0400

    comment out backlog writing

--- lib/rudder/pipeline.ex
+++ lib/rudder/pipeline.ex
@@ -22,31 +22,29 @@ defmodule Rudder.Pipeline do
     end
   end
 
-
   def process_specimen(specimen_hash, urls) do
-    # try do
-    with {:ok, specimen} <- Rudder.BlockSpecimenDiscoverer.discover_block_specimen(urls),
-         {:ok, decoded_specimen} <- Rudder.Avro.BlockSpecimenDecoder.decode(specimen),
-         {:ok, block_specimen_metadata} <- extract_block_specimen_metadata(decoded_specimen),
-         {:success, block_result_file_path} <-
-           Rudder.BlockProcessor.sync_queue(block_specimen_metadata),
-         block_result_metadata <- %Rudder.BlockResultMetadata{
-           chain_id: block_specimen_metadata.chain_id,
-           block_height: block_specimen_metadata.block_height,
-           block_specimen_hash: specimen_hash,
-           file_path: block_result_file_path
-         },
-         {:ok, cid, block_result_hash} <-
-           Rudder.BlockResultUploader.upload_block_result(block_result_metadata),
-         :ok <- File.rm(block_result_file_path) do
-      {:ok, cid, block_result_hash}
-    else
-      err -> write_to_backlog(specimen_hash, urls, err)
+    try do
+      with {:ok, specimen} <- Rudder.BlockSpecimenDiscoverer.discover_block_specimen(urls),
+           {:ok, decoded_specimen} <- Rudder.Avro.BlockSpecimenDecoder.decode(specimen),
+           {:ok, block_specimen_metadata} <- extract_block_specimen_metadata(decoded_specimen),
+           {:success, block_result_file_path} <-
+             Rudder.BlockProcessor.sync_queue(block_specimen_metadata),
+           block_result_metadata <- %Rudder.BlockResultMetadata{
+             chain_id: block_specimen_metadata.chain_id,
+             block_height: block_specimen_metadata.block_height,
+             block_specimen_hash: specimen_hash,
+             file_path: block_result_file_path
+           },
+           {:ok, cid, block_result_hash} <-
+             Rudder.BlockResultUploader.upload_block_result(block_result_metadata),
+           :ok <- File.rm(block_result_file_path) do
+        {:ok, cid, block_result_hash}
+      else
+        err -> write_to_backlog(specimen_hash, urls, err)
+      end
+    rescue
+      e -> write_to_backlog(specimen_hash, urls, e)
     end
-
-    # rescue
-    #   e -> write_to_backlog(specimen_hash, urls, e)
-    # end
   end
 
   defp extract_block_specimen_metadata(decoded_specimen) do
@@ -75,8 +73,8 @@ defmodule Rudder.Pipeline do
   end
 
   defp write_to_backlog(specimen_hash, urls, _err) do
-    backlog_filepath = Application.get_env(:rudder, :backlog_filepath)
-    text = specimen_hash <> "," <> List.to_string(urls) <> ";"
-    Rudder.Util.append_to_file(text, backlog_filepath)
+    # backlog_filepath = Application.get_env(:rudder, :backlog_filepath)
+    # text = specimen_hash <> "," <> List.to_string(urls) <> ";"
+    # Rudder.Util.append_to_file(text, backlog_filepath)
   end
 end
