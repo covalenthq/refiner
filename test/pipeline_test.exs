784a1d7cbdd8a827501f2224a449890d17f6af36
kitti-katy <kitti-katy@mail.ru>
2022-11-04 15:32:15 -0400

    add pipeline tests

--- test/pipeline_test.exs
+++ test/pipeline_test.exs
@@ -15,35 +15,21 @@ defmodule Rudder.PipelineTest do
     assert block_result_hash == expected_block_result_hash
   end
 
-  # test "pipeline spawner starts pipeline processes", %{} do
-  #   test_urls = ["ipfs://QmUmd2Eme1ZNnJ8nSSmhtwnTeNdwW39YMszBug4oKnaBZM"]
-  #   {:ok, agent} = Rudder.Pipeline.Spawner.push_hash("hash1", test_urls)
+  test "pipeline spawner starts pipeline processes", %{} do
+    test_urls = ["ipfs://QmQVxzXp5DfipnwXJdXmvmXhUv9prpivsgNuZD9CZ2Q3EN"]
 
-  #   # expected_cid = "bafkreiawcv77lmfzm4wl2tmhtxx6zgkjo6eabxufjhulnbc4zk7pbg7ciy"
+    {:ok, agent} = Rudder.Pipeline.Spawner.push_hash("hash1", test_urls)
 
-  #   # expected_block_result_hash =
-  #   #   <<22, 21, 127, 245, 176, 185, 103, 44, 189, 77, 135, 157, 239, 236, 153, 73, 119, 136, 0,
-  #   #     222, 133, 73, 232, 182, 132, 92, 202, 190, 240, 155, 226, 70>>
+    expected_cid = "bafybeiav76c7cwycbazgzfc75cg56khknabahw3npuizpftnks6adnrwjy"
 
-  #   # {status, cid, block_result_hash} = Agent.get(agent, & &1)
-  #   IO.inspect(Agent.get(agent, & &1))
+    expected_block_result_hash =
+      <<1, 156, 207, 30, 71, 119, 21, 41, 176, 249, 241, 222, 201, 7, 88, 135, 8, 168, 81, 117,
+        229, 149, 4, 45, 107, 147, 243, 250, 69, 107, 112, 159>>
 
-  #   # assert status == :ok
-  #   # assert cid == expected_cid
-  #   # assert block_result_hash == expected_block_result_hash
-  # end
+    {status, cid, block_result_hash} = Agent.get(agent, & &1)
 
-  # test "pipeline spawner writes to backlog with invalid urls", %{} do
-  #   test_urls = ["ipfs://QmR4BQi8fTdZM28GuWmtfjkbNPPzxiHCBNRXJng6rSEfcv"]
-  #   r = Rudder.Pipeline.Spawner.push_hash("hash1", [])
-  #   IO.inspect(r)
-  #   # IO.inspect(Agent.get(agent2, & &1))
-  #   # # => %{}
-  #   IO.inspect(DynamicSupervisor.count_children(Rudder.Pipeline.Spawner))
-  #   #     {status, cid, block_result_hash} = Rudder.Pipeline.pipeline("hash", [])
-  # end
-
-  # test "generates a specimen backlog file with the failed specimen hash", %{} do
-  #   {status, cid, block_result_hash} = Rudder.Pipeline.pipeline("hash", [])
-  # end
+    assert status == :ok
+    assert cid == expected_cid
+    assert block_result_hash == expected_block_result_hash
+  end
 end
