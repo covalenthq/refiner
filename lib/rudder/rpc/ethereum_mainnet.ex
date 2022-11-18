34a3a4771ddbad901fe51522ad73519ef6aa7cc6
Pranay Valson <pranay.valson@gmail.com>
2022-11-18 14:08:53 +0530

    update chain_id static and dynamic
    Signed-off-by: Pranay Valson <pranay.valson@gmail.com>

--- lib/rudder/rpc/ethereum_mainnet.ex
+++ lib/rudder/rpc/ethereum_mainnet.ex
@@ -2,7 +2,7 @@ defmodule Rudder.Network.EthereumMainnet do
   use Rudder.RPC.EthereumClient,
     otp_app: :rudder,
     client_opts: Application.get_env(:rudder, :proofchain_node),
-    chain_id: 1,
+    chain_id: Application.get_env(:rudder, :proofchain_chain_id),
     description: "Ethereum Foundation Mainnet",
     currency: [name: "Ether", ticker_symbol: "ETH"]
 end
