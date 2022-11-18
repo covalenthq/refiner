063cb6bb16f20b0d95b8da07adcac5d5337572df
kitti-katy <kitti-katy@mail.ru>
2022-11-17 19:04:00 -0500

    move chain id to config files

--- config/test.exs
+++ config/test.exs
@@ -2,4 +2,5 @@ import Config
 
 config :rudder,
   operator_private_key: "8b3a350cf5c34c9194ca85829a2df0ec3153be0318b5e2d3348e872092edffba",
-  proofchain_address: "0xCF3d5540525D191D6492F1E0928d4e816c29778c"
+  proofchain_address: "0xCF3d5540525D191D6492F1E0928d4e816c29778c",
+  proofchain_chain_id: 1
