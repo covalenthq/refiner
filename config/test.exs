9bf5e9d901bf1fcd49eeb3135c06097c5d2ffef3
Pranay Valson <pranay.valson@gmail.com>
2022-12-02 10:38:58 +0530

    tentative comment for test.exs
    Signed-off-by: Pranay Valson <pranay.valson@gmail.com>

--- config/test.exs
+++ config/test.exs
@@ -1,7 +1,7 @@
 import Config
 
 config :rudder,
-  operator_private_key: "8b3a350cf5c34c9194ca85829a2df0ec3153be0318b5e2d3348e872092edffba",
+  operator_private_key: "8b3a350cf5c34c9194ca85829a2df0ec3153be0318b5e2d3348e872092edffba", #docker test keys can be exposed
   proofchain_address: "0xCF3d5540525D191D6492F1E0928d4e816c29778c",
   proofchain_chain_id: 31337,
   proofchain_node: "http://hardhat-node:8545"
