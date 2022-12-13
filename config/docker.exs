38d7bc43e82b5286f8c3baa9bec5e5bc0af6c977
Pranay Valson <pranay.valson@gmail.com>
2022-12-13 15:54:34 +0530

    hardcode docker test requirements
    Signed-off-by: Pranay Valson <pranay.valson@gmail.com>

--- config/docker.exs
+++ config/docker.exs
@@ -4,5 +4,5 @@ config :rudder,
   operator_private_key: "8b3a350cf5c34c9194ca85829a2df0ec3153be0318b5e2d3348e872092edffba",
   proofchain_address: "0xCF3d5540525D191D6492F1E0928d4e816c29778c",
   proofchain_chain_id: 31337,
-  proofchain_node: System.get_env("NODE_ETHEREUM_MAINNET"),
-  ipfs_pinner_url: System.get_env("IPFS_PINNER_URL")
+  proofchain_node: "http://hardhat-node:8545/",
+  ipfs_pinner_url: "http://ipfs-pinner:3000"
