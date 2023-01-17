a3c1567a2431aa456a4eb3a2bd1a71469eb60e5a
Pranay Valson <pranay.valson@gmail.com>
2023-01-17 16:57:48 +0530

    add tests for file opening
    Signed-off-by: Pranay Valson <pranay.valson@gmail.com>

--- test/util_test.exs
+++ test/util_test.exs
@@ -35,6 +35,18 @@ defmodule Rudder.UtilTest do
   end
 
   test "returns 'idunno' for other types" do
-    assert Rudder.Util.typeof() == "idunno"
+    specimen_path =
+      "./test-data/encoded/1-15892728-replica-0x84a541916d6d3c974b4da961e2f00a082c03a071132f5db27787124597c094c1"
+    {:ok, decoded_specimen} = Rudder.Avro.BlockSpecimenDecoder.decode_file(specimen_path)
+
+    assert Rudder.Util.typeof(decoded_specimen) == "idunno"
+  end
+
+  test "get_file_paths returns a list of files in the given directory" do
+    assert Rudder.Util.get_file_paths("./test-data/block-specimen/*") == ["test-data/block-specimen/15892728.specimen.json", "test-data/block-specimen/15892740.specimen.json", "test-data/block-specimen/15892755.specimen.json"]
+  end
+
+  test "get_file_paths returns an empty list when given a directory with no files or invalid path" do
+    assert Rudder.Util.get_file_paths("./evm") == []
   end
 end
