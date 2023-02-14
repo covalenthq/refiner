0b9ef989d541c643e5fb6920a02188c6f1318eb0
Pranay Valson <pranay.valson@gmail.com>
2023-02-14 15:05:54 -0800

    refactor to use ets tables for metrics state
    Signed-off-by: Pranay Valson <pranay.valson@gmail.com>

--- lib/rudder/metrics/telemetry.ex
+++ lib/rudder/metrics/telemetry.ex
@@ -16,18 +16,45 @@ defmodule Rudder.Telemetry do
 
   defp metrics do
     [
-      # event_emitting
-      counter("rudder.events.emit.value"),
-      sum("rudder.events.emit.value"),
-      # summary("rudder.events.emit.value"),
       # ipfs_pinning
       counter("rudder.events.ipfs_pin.value"),
       sum("rudder.events.ipfs_pin.value"),
       # summary("rudder.events.ipfs_pin.value"),
+
       # ipfs_fetching
       counter("rudder.events.ipfs_fetch.value"),
-      sum("rudder.events.ipfs_fetch.value")
-      # summary("rudder.events.ipfs_fetch.value")
+      sum("rudder.events.ipfs_fetch.value"),
+      # summary("rudder.events.ipfs_fetch.value"),
+
+      # bsp_decoding
+      counter("rudder.events.bsp_decode.value"),
+      sum("rudder.events.bsp_decode.value"),
+      # summary("rudder.events.bsp_decode.value"),
+
+      # bsp_extracting
+      counter("rudder.events.bsp_extract.value"),
+      sum("rudder.events.bsp_extract.value"),
+      # summary("rudder.events.bsp_extract.value"),
+
+      # evm_executing
+      counter("rudder.events.evm_execute.value"),
+      sum("rudder.events.evm_execute.value"),
+      # summary("rudder.events.evm_execute.value"),
+
+      # brp_uploading
+      counter("rudder.events.bsp_upload.value"),
+      sum("rudder.events.bsp_upload.value"),
+      # summary("rudder.events.bsp_upload.value"),
+
+      # tx_proofing
+      counter("rudder.events.tx_proof.value"),
+      sum("rudder.events.tx_proof.value"),
+      # summary("rudder.events.tx_proof.value"),
+
+      # brp_pipelining
+      counter("rudder.events.brp_pipeline.value"),
+      sum("rudder.events.brp_pipeline.value")
+      # summary("rudder.events.brp_pipeline.value")
     ]
   end
 end
