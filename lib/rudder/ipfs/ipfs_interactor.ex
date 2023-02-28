31fa55bed8972e50dd6dadce78b4071b0105a055
Sudeep <sudeepdino008@gmail.com>
2023-02-28 16:18:16 +0530

    increases timeout for ipfs fetch
    - the ipfs-pinner now has a http gateway roster which it goes
      through finding which gateway can fetch content in reasonable
      time. The timeout here needs to be big enough to allow going
      through the roster.

--- lib/rudder/ipfs/ipfs_interactor.ex
+++ lib/rudder/ipfs/ipfs_interactor.ex
@@ -58,7 +58,7 @@ defmodule Rudder.IPFSInteractor do
 
     {:ok, %Finch.Response{body: body, headers: _, status: _}} =
       Finch.build(:get, "#{ipfs_url}/get?cid=#{cid}")
-      |> Finch.request(Rudder.Finch, receive_timeout: 120_000_000, pool_timeout: 120_000_000)
+      |> Finch.request(Rudder.Finch, receive_timeout: 150_000_000, pool_timeout: 150_000_000)
 
     end_fetch_ms = System.monotonic_time(:millisecond)
     Events.ipfs_fetch(end_fetch_ms - start_fetch_ms)
