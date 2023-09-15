9f97fe6a2ce00928176dfc37bf2f9ae43e5c72a2
Pranay Valson <pranay.valson@gmail.com>
2023-09-15 11:49:27 -0700

    add rustler to force ci build
    Signed-off-by: Pranay Valson <pranay.valson@gmail.com>

--- config/config.exs
+++ config/config.exs
@@ -38,6 +38,7 @@ config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
 
+config :rustler_precompiled, :force_build, ex_keccak: true
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env()}.exs"
