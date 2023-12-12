00555c72d65250007bde3b386ce12694bef417eb
Pranay Valson <pranay.valson@gmail.com>
2023-12-12 14:12:43 -0800

    update rudder minor version
    Signed-off-by: Pranay Valson <pranay.valson@gmail.com>

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Rudder.MixProject do
   def project do
     [
       app: :rudder,
-      version: "0.2.15",
+      version: "0.2.17",
       elixir: "~> 1.14.3",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
