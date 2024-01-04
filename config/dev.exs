bce44da671068ca4628cf2fa8f7e9e0d87d46818
Pranay Valson <pranay.valson@gmail.com>
2024-01-04 15:44:02 -0800

    fix docker compose run command
    Signed-off-by: Pranay Valson <pranay.valson@gmail.com>

--- config/dev.exs
+++ config/dev.exs
@@ -50,7 +50,7 @@ config :phoenix, :plug_init_mode, :runtime
 # Default these should point to moonbase alpha since we do all the development testing there
 config :rudder,
   operator_private_key: System.get_env("BLOCK_RESULT_OPERATOR_PRIVATE_KEY"),
-  bsp_proofchain_address: "0x1BFa3b5E9bE2c5298B7DE11B5Acb08c37683f4eF",
+  bsp_proofchain_address: "0x30F220B44E937dd6A2A43D91D564E259f3574eb8",
   brp_proofchain_address: "0x06A38f70c8e245e1cfe1b7d5ACa1B7cd29380343",
   proofchain_chain_id: 1287,
   proofchain_node: System.get_env("NODE_ETHEREUM_MAINNET"),
