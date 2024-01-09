5007fd9f90311c19cd23357371399844d3a483b3
Pranay Valson <pranay.valson@gmail.com>
2024-01-09 15:19:55 -0800

    adjust extracted log data signature
    Signed-off-by: Pranay Valson <pranay.valson@gmail.com>

--- lib/rudder/proof_chain/block_specimen_event_listener.ex
+++ lib/rudder/proof_chain/block_specimen_event_listener.ex
@@ -64,7 +64,7 @@ defmodule Rudder.ProofChain.BlockSpecimenEventListener do
       {:ok, [_event_hash, chain_id_raw, block_height_raw, block_hash]} =
         Map.fetch(log_event, "topics")
 
-      [specimen_hash_raw] = Rudder.Util.extract_data(log_event, "(bytes32)")
+      [_validator_bit_map, specimen_hash_raw] = Rudder.Util.extract_data(log_event, "(uint256,bytes32)")
 
       # prepare data to generate key
       specimen_hash = Base.encode16(specimen_hash_raw, case: :lower)
