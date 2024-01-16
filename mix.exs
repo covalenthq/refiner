547f6c6402163a144f078e20b19b431d818ee1f8
Pranay Valson <pranay.valson@gmail.com>
2024-01-16 13:49:17 -0800

    push brp-refiner v0.4.0 for cqt-staking v1.5
    Signed-off-by: Pranay Valson <pranay.valson@gmail.com>

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Rudder.MixProject do
   def project do
     [
       app: :rudder,
-      version: "0.3.1",
+      version: "0.4.0",
       elixir: "~> 1.14.3",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
