5a3f07288ea5f9d53d417fc022b70b086a1f685c
sudeep <sudeepdino008@gmail.com>
2024-03-07 16:01:17 +0530

    use compile_env instead of get_env to track changes in env variables

--- lib/refiner/rpc/ethereum_mainnet.ex
+++ lib/refiner/rpc/ethereum_mainnet.ex
@@ -1,8 +1,8 @@
 defmodule Refiner.Network.EthereumMainnet do
   use Refiner.RPC.EthereumClient,
     otp_app: :refiner,
-    client_opts: Application.get_env(:refiner, :proofchain_node),
-    chain_id: Application.get_env(:refiner, :proofchain_chain_id),
+    client_opts: Application.compile_env(:refiner, :proofchain_node),
+    chain_id: Application.compile_env(:refiner, :proofchain_chain_id),
     description: "Ethereum Foundation Mainnet",
     currency: [name: "Ether", ticker_symbol: "ETH"]
 end
