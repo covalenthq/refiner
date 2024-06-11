8b3247b0557930c159fe0de56db5cddd5ccf3c20
Pranay Valson <pranay.valson@gmail.com>
2024-06-10 21:10:53 -0700

    bump refiner minor version
    Signed-off-by: Pranay Valson <pranay.valson@gmail.com>

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Refiner.MixProject do
   def project do
     [
       app: :refiner,
-      version: "0.5.0",
+      version: "0.5.2",
       elixir: "~> 1.14.3",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
